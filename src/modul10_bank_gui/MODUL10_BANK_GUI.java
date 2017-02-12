1ccb742a31e03bd58ebf8b107839efd10cbe713d
cikaandhini <begidakancika120200@gmail.com>
2017-02-12 12:58:43 +0700

    Final Bank GUI

---
+++ src/modul10_bank_gui/MODUL10_BANK_GUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul10_bank_gui;
+
+/**
+ *
+ * @author cikaandhini
+ */
+public class MODUL10_BANK_GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
